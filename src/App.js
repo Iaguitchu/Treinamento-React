f0d6af21b115c8da5905b76544b8c79c5103bf16
iago <iago@lakatos>
2025-02-03 10:24:55 -0300

    limpando o projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './componentes/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          salve <code>src/App.js</code> salvado.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
     </div>
   );
 }

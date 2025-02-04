bbb178d2b98bfeb761561e5cc1582d62537c1b6f
iago <iago@lakatos>
2025-02-04 17:22:04 -0300

    adicionando css e lista usando props

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,16 +6,6 @@ import { useState } from 'react'
 
 
 const Formulario = (props) => {
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'Ux e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
-
     const aoSalvar = (evento) =>{
         evento.preventDefault()
         props.aoColaboradorCadastrado({
@@ -26,6 +16,7 @@ const Formulario = (props) => {
         })
     }
 
+    console.log("estou aqui")
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
@@ -35,11 +26,11 @@ const Formulario = (props) => {
     return (
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
-                <h2>Preencha os dados para cirar o card do colaborador</h2>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto obrigatorio = {true} label="Nome" placeholder="Digite seu nome" valor = {nome} aoAlterado = {valor=> setNome(valor)} />
                 <CampoTexto obrigatorio = {true}label="Cargo" placeholder="Digite seu cargo" valor = {cargo} aoAlterado = {valor=> setCargo(valor)}/>
                 <CampoTexto obrigatorio = {true}label="Imagem" placeholder="Digite o endereço da imagem" valor = {imagem} aoAlterado = {valor=> setImagem(valor)}/>
-                <ListaSuspensa obrigatorio = {true}label = "Time" itens = {times} valor={time} aoAlterado = {valor => setTime(valor)}/>
+                <ListaSuspensa obrigatorio = {true}label = "Time" itens = {props.times} valor={time} aoAlterado = {valor => setTime(valor)}/>
                 <Botao> Criar Card </Botao>
             </form>
         </section>

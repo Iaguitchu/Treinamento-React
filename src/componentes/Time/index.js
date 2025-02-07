aa8c971244ce8ab68d25afda1ce8962897eaf1b0
iago <iago@lakatos>
2025-02-07 10:09:23 -0300

    corrigindo bug de lista e adionando cor aos cards

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -2,11 +2,12 @@ import Colaborador from '../Colaborador'
 import'./Time.css'
 
 const Time = (props) => {
+     const css = { backgroundColor: props.corSecundaria }
     return(
         (props.colaboradores.length > 0) && <section className='time' style={{backgroundColor: props.corSecundaria}}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
-                {props.colaboradores.map( colaborador => <Colaborador nome={colaborador.nome}
+                {props.colaboradores.map( colaborador => <Colaborador corDeFundo={props.corPrimaria} nome={colaborador.nome}
                 cargo = {colaborador.cargo}
                 imagem = {colaborador.imagem}
                 />)}
